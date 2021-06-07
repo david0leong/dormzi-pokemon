25fef3dd8e9a70f2e502b847f4d213a832a71984
David Leong <david0leong@gmail.com>
2021-06-07 12:14:23 -0700

    Add NotFound component

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => (
+  <div
+    className="d-flex flex-column justify-content-center align-items-center not-found-wrapper"
+    style={{ minHeight: '400px' }}
+  >
+    <h1>404</h1>
+    <h3>Not Found</h3>
+  </div>
+)
+
+export default NotFound
